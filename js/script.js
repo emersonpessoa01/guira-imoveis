c8de315112d7e2e00f4c967b1190f5b763562838
Emerson Pessoa <60630092+emersonpessoa01@users.noreply.github.com>
2022-09-11 21:51:58 -0300

    added clip :sparkles:

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
 let menu = document.querySelector("#menu-btn");
 let navbar = document.querySelector(".navbar");
 let header = document.querySelector(".header");
+let clip = document.querySelectorAll(".clip");
+console.log(clip);
 
 window.addEventListener("scroll", () => {
   header.classList.toggle("shadow", window.scrollY > 0);
-  navbar.classList.remove("active")
+  navbar.classList.remove("active");
 });
 
 menu.onclick = () => {
@@ -12,39 +14,12 @@ menu.onclick = () => {
   navbar.classList.toggle("active");
 };
 
-const botao = document.querySelector("#botao");
+for (let i = 0; i < clip.length; i++) {
+  clip[i].onmouseenter = () => {
+    clip[i].play();
 
-botao.addEventListener("click", (event) => {
-  //desabilita a funcao padrão do botao
-  event.preventDefault();
-  //define as variaveis dos inputs
-  const userEmail = document.querySelector("#email");
-  const mensagem = document.querySelector("#mensagem");
-
-  const [user] = userEmail.value.split("@");
-
-  //define as variaveis dos "spans" usados para mensagem de erro e sucesso
-  const spanEmail = document.querySelector("#spanEmail");
-  const spanMensagem = document.querySelector("#spanMensagem");
-  const textoSucesso = document.querySelector("#textoSucesso");
-
-  //define o regex do email
-  const regTeste = /^[a-z0-9A-Z.]{1,32}@[a-z0-9]{1,16}\.[a-z]+$/;
-
-  //verifica se os campos estao de acordo com a validacao
-  if (userEmail.value === "") {
-    spanEmail.classList.add("erro");
-    textoSucesso.innerContent = "";
-  } else if (!regTeste.test(userEmail.value)) {
-    spanEmail.classList.add("erro");
-    textoSucesso.innerContent = "";
-  } else if (mensagem.value === "") {
-    spanMensagem.classList.add("erro");
-    spanEmail.classList.remove("erro");
-    textoSucesso.innerContentt = "";
-  } else {
-    spanMensagem.classList.remove("erro");
-    spanEmail.classList.remove("erro");
-    textoSucesso.innerText = `Obrigado pelo contato, ${user}!`;
-  }
-});
+  };
+  clip[i].onmouseout = () => {
+    clip[i].pause();
+  };
+}
