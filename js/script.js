4d095732207649a7e31279c64e40da1cae3b915a
Emerson Pessoa <60630092+emersonpessoa01@users.noreply.github.com>
2022-09-15 10:17:53 -0300

    comentado clip :sparkles:

--- js/script.js
+++ js/script.js
@@ -1,12 +1,13 @@
 let menu = document.querySelector("#menu-btn");
 let navbar = document.querySelector(".navbar");
 let header = document.querySelector(".header");
-let clip = document.querySelectorAll(".clip");
-console.log(clip);
+// let clip = document.querySelectorAll(".clip");
+// console.log(clip);
 
 window.addEventListener("scroll", () => {
   header.classList.toggle("shadow", window.scrollY > 0);
   navbar.classList.remove("active");
+  menu.classList.remove("fa-times");
 });
 
 menu.onclick = () => {
@@ -14,12 +15,12 @@ menu.onclick = () => {
   navbar.classList.toggle("active");
 };
 
-for (let i = 0; i < clip.length; i++) {
-  clip[i].onmouseenter = () => {
-    clip[i].play();
+// for (let i = 0; i < clip.length; i++) {
+//   clip[i].onmouseenter = () => {
+//     clip[i].play();
 
-  };
-  clip[i].onmouseout = () => {
-    clip[i].pause();
-  };
-}
+//   };
+//   clip[i].onmouseout = () => {
+//     clip[i].pause();
+//   };
+// }
